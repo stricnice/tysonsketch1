723a758c03b7014153e4ebee447050af6e4fc9b1
stricnice <marshtides@gmail.com>
2017-04-01 12:27:42 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,13 @@
-function setup() {
-
-	createCanvas(2000,2000);
-	background (50);
-
-
+function setup(){
+	createCanvas(600,400);
+	background(200);
+	
 }
 
-function draw(){
-
-	fill(300,0,0,40);
+function draw (){
+	
+	fill(200,0,0,50);
 	noStroke();
 	ellipse(mouseX,mouseY,30,30);
-
+	
 }
